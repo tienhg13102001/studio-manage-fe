7a47354037098329128b84c567999f3b9574c4e9
Bryan_Tien <tienhg201@gmail.com>
2026-04-16 11:04:23 +0700

    Please provide the code changes or file diffs you would like me to summarize.

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,24 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // React core
+          'vendor-react': ['react', 'react-dom', 'react-router-dom', 'react-redux', 'react-is'],
+          // Redux
+          'vendor-redux': ['@reduxjs/toolkit'],
+          // Chart (nặng ~500kB)
+          'vendor-charts': ['recharts'],
+          // Excel export (nặng ~800kB)
+          'vendor-excel': ['exceljs', 'xlsx'],
+          // Utilities
+          'vendor-utils': ['axios', 'date-fns', 'react-hook-form', 'react-toastify'],
+        },
+      },
+    },
+  },
 })
+
