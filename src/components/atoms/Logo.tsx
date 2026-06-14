875d893a89a256bee8403ad9773e8dd59d883209
Bryan_Tien <tienhg201@gmail.com>
2026-06-14 16:07:41 +0700

    fix

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,5 +1,4 @@
-// Ảnh phục vụ tĩnh từ public/ (Vite copy nguyên trạng, không qua bước transform/hash)
-const logoSrc = '/images/logo.webp';
+import logoSrc from '../../assets/images/logo.webp';
 
 interface LogoProps {
   size?: number;
